f5c1c4466445b094a05018af4cb933cea644735d
Stanislav <staspovolotskiy@gmail.com>
2020-06-01 17:25:13 +0300

    added check for internet connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:hive/hive.dart';
 import 'package:newsappflutter/models/article.dart';
 import 'tabs/tabs_page.dart';
@@ -23,7 +25,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void dispose() {
     Hive.close();
-//    Hive.box("articles").compact();
     super.dispose();
   }
 
@@ -39,16 +40,81 @@ class _MyAppState extends State<MyApp> {
         home: FutureBuilder(
           future: Hive.openBox("articles"),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError)
-                return Text(snapshot.error.toString());
-              else
-                return TabsPage();
-            }
-            // Although opening a Box takes a very short time,
-            // we still need to return something before the Future completes.
-            else
-              return Scaffold();
+            return OfflineBuilder(
+              connectivityBuilder: (BuildContext context,
+                  ConnectivityResult connectivity, Widget child) {
+                final bool connected = connectivity != ConnectivityResult.none;
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 32, 0, 0),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      child,
+                      Positioned(
+                        left: 0.0,
+                        right: 0.0,
+                        height: 32.0,
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          color: connected ? primaryBlack : Color(0xFFEE4400),
+                          child: connected
+                              ? Visibility(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        "Online",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 14),
+                                      ),
+                                    ],
+                                  ),
+                                  visible: false,
+                                )
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      "Offline",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 14),
+                                    ),
+                                    SizedBox(
+                                      width: 8.0,
+                                    ),
+                                    SizedBox(
+                                      width: 12.0,
+                                      height: 12.0,
+                                      child: CircularProgressIndicator(
+                                        strokeWidth: 2.0,
+                                        valueColor:
+                                            AlwaysStoppedAnimation<Color>(
+                                                Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Builder(
+                builder: (context) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError)
+                      return Text(snapshot.error.toString());
+                    else
+                      return TabsPage();
+                  }
+                  // Although opening a Box takes a very short time,
+                  // we still need to return something before the Future completes.
+                  else
+                    return Scaffold();
+                },
+              ),
+            );
           },
         ));
   }
